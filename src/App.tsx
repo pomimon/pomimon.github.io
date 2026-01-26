77a3a13792018b45a90e038863048193d5769fec
pomimon <bright.dusk1649@fastmail.com>
2026-01-26 14:25:41 -0800

    Move DaisyUi components out of html file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./index.css";
+import { Hero } from "./components/Hero";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 
 export function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
