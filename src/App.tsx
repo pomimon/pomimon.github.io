c3b25c11cc0ca724c8ba5f523a2192f4430b9727
pomimon <bright.dusk1649@fastmail.com>
2026-01-26 17:05:28 -0800

    Add Card component to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./index.css";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
+import { Card } from "./components/Card";
 
 export function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
       <Footer />
     </div>
   );
